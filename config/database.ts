5a6d2ee7bdc8c15a9a31f8d95dbd90d6adfdd609
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-12-07 16:03:41 -0300

    config: deploy strapi on railway

--- config/database.ts
+++ config/database.ts
@@ -1,14 +1,13 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false),
+      user: env('DATABASE_USER', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      ssl: env.bool(true),
     },
-    useNullAsDefault: true,
   },
 });
